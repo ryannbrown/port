71bb9e92b205a6715d5780a6bd32bc16ba0eeb5a
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-11-27 07:35:57 -0500

    Updates

---
+++ precache-manifest.edc4b67f2d695afbeecca8cb7e9c1c86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1bb30cd83f48acbd92957b1e04e960",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "de8ad8377552b254de46",
+    "url": "/portfolio/static/css/main.b5f5163c.chunk.css"
+  },
+  {
+    "revision": "df2a6aa359e434a8e83a",
+    "url": "/portfolio/static/js/2.d512330c.chunk.js"
+  },
+  {
+    "revision": "de8ad8377552b254de46",
+    "url": "/portfolio/static/js/main.80a22700.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "97b7d7c7a97345ac2b708f56cfb191af",
+    "url": "/portfolio/static/media/Ryan.97b7d7c7.jpg"
+  }
+]);
